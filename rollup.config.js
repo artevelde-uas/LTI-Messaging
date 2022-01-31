ee000f5f0df9b1cb53b5ed4a33066a8a942974cc
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-01-31 11:52:55 +0100

    Add extra output types

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,21 @@ import babel from '@rollup/plugin-babel';
 
 export default {
     input: 'src/tool-provider.js',
-    output: {
-        file: 'dist/tool-provider.js',
-        format: 'iife',
-        name: 'toolProvider'
-    },
+    output: [
+        {
+            file: 'dist/tool-provider.js',
+            format: 'iife',
+            name: 'toolProvider'
+        },
+        {
+            file: 'dist/tool-provider.cjs.js',
+            format: 'cjs'
+        },
+        {
+            file: 'dist/tool-provider.esm.js',
+            format: 'esm'
+        }
+    ],
     plugins: [
         babel({
             babelrc: false,
